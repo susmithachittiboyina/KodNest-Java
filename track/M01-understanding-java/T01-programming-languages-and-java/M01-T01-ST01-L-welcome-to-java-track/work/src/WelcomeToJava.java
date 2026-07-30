78ab53d4834d5ef5f21bd100bd66341d1fc78094
susmithachittiboyina <susmithachittiboyina5@gmail.com>
2026-07-30 12:47:51 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,9 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("Wwlcome to the Java Track");
+        System.out.println("I already know how to build programmimng logic.");
+        System.out.println("Now I am learning how to express that logic using Java.");
     }
 }
