9a0efecf6ac1b306453b07473ca86984450656ad
susmithachittiboyina <susmithachittiboyina5@gmail.com>
2026-08-04 12:32:17 +0530

    PositiveorNot.java

---
+++ track/M02/T01/ST01/P6/PositiveorNot.java
@@ -0,0 +1,14 @@
+
+public class PositiveorNot {
+
+    public static void main(String[] args) {
+        int num = -20;
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
