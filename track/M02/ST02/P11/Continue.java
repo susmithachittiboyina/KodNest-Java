9c6019198892157fa596cd1f3ef1f49629aa6331
susmithachittiboyina <susmithachittiboyina5@gmail.com>
2026-08-06 12:54:49 +0530

    NestedPgm1.java

--- track/M02/ST02/P11/Continue.java
+++ track/M02/ST02/P11/Continue.java
@@ -4,7 +4,7 @@ public class Continue {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             if (i == 6) {
-                Continue;
+                continue;
             }
             System.out.println(i);
         }
