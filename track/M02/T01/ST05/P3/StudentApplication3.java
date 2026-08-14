75808469b301828a8da9c590b0a68071c4ce19c8
susmithachittiboyina <susmithachittiboyina5@gmail.com>
2026-08-14 11:12:30 +0530

    StudentApplication3.java

---
+++ track/M02/T01/ST05/P3/StudentApplication3.java
@@ -0,0 +1,28 @@
+
+class Student {
+
+    String name;
+    int age;
+    double height;
+
+    void input(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
+
+class StudentApplication3 {
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.input("Rani", 18, 5.0);
+        s1.display();
+    }
+}
