87e475d657e36c14f78fc7647044f5d4bdb9ae2d
susmithachittiboyina <susmithachittiboyina5@gmail.com>
2026-08-19 21:49:24 +0530

    ArrayProblemSolving.java

--- track/M02/T01/ST08/P11/ArrayProblemSolving.java
+++ track/M02/T01/ST08/P11/ArrayProblemSolving.java
@@ -11,26 +11,30 @@ public class ArrayProblemSolving {
         int[] array = new int[n];
 
         int arrayTotal = 0;
+
         for (int i = 0; i < n; i++) {
             array[i] = sc.nextInt();
             arrayTotal += array[i];
-            int rows = sc.nextInt();
-            int cols = sc.nextInt();
-
-            int[][] matrix = new int[rows][cols];
-
-            int matrixTotal = 0;
-
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
-                    matrix[i][j] = sc.nextInt();
-                    matrixTotal += matrix[i][j];
-                }
-            }
-
-            System.out.println("Array total: " + arrayTotal);
-            System.out.println("Matrix total: " + matrixTotal);
-
-            sc.close();
         }
+
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        int matrixTotal = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = sc.nextInt();
+                matrixTotal += matrix[i][j];
+            }
+        }
+
+        // Display results
+        System.out.println("Array total: " + arrayTotal);
+        System.out.println("Matrix total: " + matrixTotal);
+
+        sc.close();
     }
+}
